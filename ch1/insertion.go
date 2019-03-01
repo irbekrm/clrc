2045ee76b3706ffb84a92cca1e1a83cf76f00957
Irbe Krumina <irbekrm@gmail.com>
2019-03-01 05:46:32 +0000

    add insertion sort for sorting in increasing order

--- ch1/insertion.go
+++ ch1/insertion.go
@@ -1,11 +1,22 @@
 package ch1
 
 // Sorts a slice passed as argument in descending order.
-func InsertionSort(unsorted []int) {
+func SortDec(unsorted []int) {
 	var i, j, key int
 	for j = 1; j < len(unsorted); j++ {
 		key = unsorted[j]
 		for i = j - 1; i >= 0 && unsorted[i] < key; i-- {
+			unsorted[i+1] = unsorted[i]
+		}
+		unsorted[i+1] = key
+	}
+}
+
+func SortInc(unsorted []int) {
+	var i, j, key int
+	for j = 1; j < len(unsorted); j++ {
+		key = unsorted[j]
+		for i = j - 1; i >= 0 && unsorted[i] > key; i-- {
 			unsorted[i+1] = unsorted[i]
 		}
 		unsorted[i+1] = key
